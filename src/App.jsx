f28750bed46b3f1606ed55917d9b9769f1c7ed6a
1925YSH <yashotwal777@gmail.com>
2025-07-17 00:51:32 +0530

    Fix: case-sensitive import path for Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/HOme';
+import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
